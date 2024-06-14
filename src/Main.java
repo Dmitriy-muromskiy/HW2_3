b01e0b9073b3e79fbd06f16157b5511dbc8f699a
79206 <serdukovd.a.murom@yandex.ru>
2024-06-14 07:17:46 +0300

    fixes on commit task

--- src/Main.java
+++ src/Main.java
@@ -20,14 +20,4 @@ public class Main {
     }
 }
 
-class WrongLoginException extends Exception {
-    public WrongLoginException(String message) {
-        super(message);
-    }
-}
 
-class WrongPasswordException extends Exception {
-    public WrongPasswordException(String message) {
-        super(message);
-    }
-}
